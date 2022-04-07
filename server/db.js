32ff339a95e409f01e9ae3feef98ee3ed4e6110b
Ibrahim Patrawala <ibupatra@gmail.com>
2022-04-07 11:40:00 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: "postgres",
-  password: "postgres19",
+  password: "5253",
   host: "localhost",
   port: "5432",
   database: "jwtauth",
