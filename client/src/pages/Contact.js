e74d309c2a8f6d363eb641f01a657cebd201f2d0
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2022-06-13 12:42:22 +0530

    Added Contact Lists
    Rendering Issue: Doesn't render any component

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,7 +1,12 @@
 import React, { Fragment, useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+// import AppConfig from "App.config";
+
 import Nav from "../components/NavbarDashboard";
 import Sidebar from "../components/Sidebar";
 import Footer from "../components/Footer";
+import AddContact from "../components/AddContact";
+import ContactCard from "../components/ContactCard";
 // import { ButtonR } from "../components/ButtonElements";
 import "../styles/App.css";
 import "../styles/Speller.css";
@@ -45,6 +50,14 @@ const Contact = ({
   //   useEffect(() => {
   //     getName();
   //   }, []);
+
+  const contacts = useSelector((state) => state.contacts.list);
+
+  const [contactFormVisiblity, setContactFormVisiblity] = useState(false);
+
+  const toggleContactFormVisiblity = () =>
+    setContactFormVisiblity((visiblity) => !visiblity);
+
   return (
     <div className="dashboard">
       <Sidebar
@@ -65,6 +78,42 @@ const Contact = ({
       <div className="fullpage-dashboard">
         <Fragment>
           <h1>Contact</h1>
+          <div className="row">
+            <div className="col text-center">
+              <h2>Manage contacts!!!</h2>
+              <p>
+                This demo performs CRUD operation in redux store. You can add,
+                edit and delete a contact in the below demo
+              </p>
+
+              <div className="col-sm-6 offset-sm-3">
+                <div className="card">
+                  <div className="card-header bg-dark text-white text-left">
+                    <h4 className="d-inline">Contact List</h4>
+                    <span
+                      className="float-right mt-1 cursor-pointer"
+                      onClick={toggleContactFormVisiblity}
+                    >
+                      <i className="fas fa-plus"></i>
+                    </span>
+                  </div>
+
+                  <ul className="list-group list-group-flush">
+                    {contacts.map((contact, index) => (
+                      <ContactCard
+                        {...contact}
+                        index={index}
+                        key={contact.name}
+                      />
+                    ))}
+                  </ul>
+                </div>
+              </div>
+              {contactFormVisiblity && (
+                <AddContact onClose={toggleContactFormVisiblity} />
+              )}
+            </div>
+          </div>
         </Fragment>
       </div>
       <Footer />
