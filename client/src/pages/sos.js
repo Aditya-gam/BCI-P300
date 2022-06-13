79322de2b6eaae5af65a6aed0eb25ca147c73617
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2022-06-13 12:42:52 +0530

    Added geo location fetching
    Rendering issue

--- client/src/pages/sos.js
+++ client/src/pages/sos.js
@@ -7,7 +7,7 @@ import "../styles/App.css";
 import "../styles/Speller.css";
 import "../styles/Dashboard.css";
 
-const Contact = ({
+const Sos = ({
   isAuthenticated,
   setAuth,
   setRegister,
@@ -45,6 +45,17 @@ const Contact = ({
   //   useEffect(() => {
   //     getName();
   //   }, []);
+
+  const [details, setDetails] = useState(null);
+
+  const getUserGeolocationDetails = () => {
+    fetch(
+      "https://geolocation-db.com/json/0f761a30-fe14-11e9-b59f-e53803842572"
+    )
+      .then((response) => response.json())
+      .then((data) => setDetails(data));
+  };
+
   return (
     <div className="dashboard">
       <Sidebar
@@ -64,7 +75,34 @@ const Contact = ({
       />
       <div className="fullpage-dashboard">
         <Fragment>
-          <h1>Contact</h1>
+          <h1>SOS!!!</h1>
+          <div className="row">
+            <div className="col text-center">
+              <h2>Find my IP and Location</h2>
+              <p className="mt-3">
+                <button
+                  className="btn btn-primary"
+                  onClick={getUserGeolocationDetails}
+                >
+                  Find my details
+                </button>
+
+                <div className="row justify-content-center mt-3">
+                  <div className="col-lg-6 text-center text-dark">
+                    {details && (
+                      <ul className="list-group">
+                        <li className="list-group-item">
+                          Location :{" "}
+                          {`${details.city}, ${details.country_name}(${details.country_code})`}
+                        </li>
+                        <li className="list-group-item">IP: {details.IPv4}</li>
+                      </ul>
+                    )}
+                  </div>
+                </div>
+              </p>
+            </div>
+          </div>
         </Fragment>
       </div>
       <Footer />
@@ -72,4 +110,4 @@ const Contact = ({
   );
 };
 
-export default Contact;
+export default Sos;
