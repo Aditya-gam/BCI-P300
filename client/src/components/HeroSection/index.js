504c64353d2577c10e7c8c2656c3844d87fbdce0
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2022-06-17 11:43:00 +0530

    Changed tags and added css

--- client/src/components/HeroSection/index.js
+++ client/src/components/HeroSection/index.js
@@ -1,17 +1,9 @@
 import React, { useState } from "react";
 import Video from "../../videos/video3.mp4";
 import { ButtonS } from "../ButtonElements";
-import {
-  HeroContainer,
-  HeroBg,
-  VideoBg,
-  HeroH1,
-  HeroP,
-  HeroBtnWrapper,
-  ArrowForward,
-  ArrowRight,
-  HeroContent,
-} from "./HeroElements";
+import { ArrowForward, ArrowRight } from "./HeroElements";
+
+import "../../styles/HeroElements.css";
 
 const HeroSection = () => {
   const [hover, setHover] = useState(false);
@@ -22,35 +14,37 @@ const HeroSection = () => {
 
   return (
     <div>
-      <HeroContainer id="Home">
-        <HeroBg>
-          <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
-        </HeroBg>
-        <HeroContent>
-          <HeroH1 className="title-text">BCI P300</HeroH1>
-          <HeroP>
+      <div className="HeroContainer" id="Home">
+        <div className="HeroBg">
+          <video className="VideoBg" autoPlay loop muted>
+            <source src={Video} type="video/mp4"></source>
+          </video>
+        </div>
+        <div className="HeroContent">
+          <h1 className="title-text HeroH1">BCI P300</h1>
+          <p className="HeroP">
             A Brain-computer Interface (BCI) can be used as a neurofeedback
             training tool to improve cognitive performance.
-          </HeroP>
-          <HeroBtnWrapper>
+          </p>
+          <div className="HeroBtnWrapper">
             <div className="get-started">
-            <ButtonS
-              to="signup"
-              onMouseEnter={onHover}
-              onMouseLeave={onHover}
-              primary="true"
-              dark="true"
-              spy={true}
-              exact="true"
-              offset={-80}
-              activeClassName="active"
-            >
-              Get started {hover ? <ArrowForward /> : <ArrowRight />}
-            </ButtonS>
+              <ButtonS
+                to="signup"
+                onMouseEnter={onHover}
+                onMouseLeave={onHover}
+                primary="true"
+                dark="true"
+                spy={true}
+                exact="true"
+                offset={-80}
+                activeClassName="active"
+              >
+                Get started {hover ? <ArrowForward /> : <ArrowRight />}
+              </ButtonS>
             </div>
-          </HeroBtnWrapper>
-        </HeroContent>
-      </HeroContainer>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
