cda281b7e6e8812e4fa469a558849027775a092c
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2022-06-17 09:59:05 +0530

    Changed Styled Components to divs

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,83 +1,163 @@
 import React from "react";
-import {FaFacebook, FaInstagram, FaLinkedin, FaTwitter, FaYoutube} from 'react-icons/fa';
-import { animateScroll as scroll } from "react-scroll";
 import {
-  FooterContainer,
-  FooterWrap,
-  FooterLinksContainer,
-  FooterLinksWrapper,
-  FooterLinkItems,
-  FooterLinkTitle,
-  FooterLink,
-  SocialMedia,
-  SocialMediaWrap,
-  SocialLogo,
-  WebsiteRights,
-  SocialIcons,
-  SocialIconLink
-} from "./FooterElements";
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa";
+import { animateScroll as scroll } from "react-scroll";
+// import {
+//   FooterContainer,
+//   FooterWrap,
+//   FooterLinksContainer,
+//   FooterLinksWrapper,
+//   FooterLinkItems,
+//   FooterLinkTitle,
+//   FooterLink,
+//   SocialMedia,
+//   SocialMediaWrap,
+//   SocialLogo,
+//   WebsiteRights,
+//   SocialIcons,
+//   SocialIconLink,
+// } from "./FooterElements";
+
+import "../../styles/FooterElements.css";
 
 const Footer = () => {
-
   const toggleHome = () => {
     scroll.scrollToTop();
   };
 
   return (
-    <>
-      <FooterContainer>
-        <FooterWrap>
-          <FooterLinksContainer>
-            <FooterLinksWrapper>
-              <FooterLinkItems>
-                <FooterLinkTitle>About Us</FooterLinkTitle>
-                <FooterLink to="/login">How it works</FooterLink>
-                <FooterLink to="/login">Testimonials</FooterLink>
-                <FooterLink to="/login">Careers</FooterLink>
-                <FooterLink to="/login">Investors</FooterLink>
-                <FooterLink to="/login">Terms of Service</FooterLink>
-              </FooterLinkItems>
-              <FooterLinkItems>
-                <FooterLinkTitle>Contact Us</FooterLinkTitle>
-                <FooterLink to="/login">Contact</FooterLink>
-                <FooterLink to="/login">Support</FooterLink>
-                <FooterLink to="/login">Destinations</FooterLink>
-                <FooterLink to="/login">Sponsorships</FooterLink>
-              </FooterLinkItems>
-            </FooterLinksWrapper>
-            <FooterLinksWrapper>
-              <FooterLinkItems>
-                <FooterLinkTitle>Videos</FooterLinkTitle>
-                <FooterLink to="/login">Submit Video</FooterLink>
-                <FooterLink to="/login">Ambassadors</FooterLink>
-                <FooterLink to="/login">Agency</FooterLink>
-                <FooterLink to="/login">Influencer</FooterLink>
-              </FooterLinkItems>
-              <FooterLinkItems>
-                <FooterLinkTitle>Social Media</FooterLinkTitle>
-                <FooterLink to="/login">Instagram</FooterLink>
-                <FooterLink to="/login">Facebook</FooterLink>
-                <FooterLink to="/login">Youtube</FooterLink>
-                <FooterLink to="/login">Twitter</FooterLink>
-              </FooterLinkItems>
-            </FooterLinksWrapper>
-          </FooterLinksContainer>
-          <SocialMedia>
-              <SocialMediaWrap>
-                  <SocialLogo to="/" onClick={toggleHome}>BCI</SocialLogo>
-                  <WebsiteRights>BCI © {new Date().getFullYear()} All rights reserved.</WebsiteRights>
-                  <SocialIcons>
-                      <SocialIconLink href="/" target="_blank" aria-label="Facebook"><FaFacebook /></SocialIconLink>
-                      <SocialIconLink href="/" target="_blank" aria-label="Instagram"><FaInstagram /></SocialIconLink>
-                      <SocialIconLink href="/" target="_blank" aria-label="Youtube"><FaYoutube /></SocialIconLink>
-                      <SocialIconLink href="/" target="_blank" aria-label="Twitter"><FaTwitter /></SocialIconLink>
-                      <SocialIconLink href="/" target="_blank" aria-label="Linkedin"><FaLinkedin /></SocialIconLink>
-                  </SocialIcons>
-              </SocialMediaWrap>
-          </SocialMedia>
-        </FooterWrap>
-      </FooterContainer>
-    </>
+    <div>
+      <div className="FooterContainer">
+        <div className="FooterWrap">
+          <div className="FooterLinksContainer">
+            <div className="FooterLinksWrapper">
+              <div className="FooterLinkItems">
+                <div className="FooterLinkTitle">About Us</div>
+                <div className="FooterLink" to="/login">
+                  How it works
+                </div>
+                <div className="FooterLink" to="/login">
+                  Testimonials
+                </div>
+                <div className="FooterLink" to="/login">
+                  Careers
+                </div>
+                <div className="FooterLink" to="/login">
+                  Investors
+                </div>
+                <div className="FooterLink" to="/login">
+                  Terms of Service
+                </div>
+              </div>
+              <div classname="FooterLinkItems">
+                <div className="FooterLinkTitle">Contact Us</div>
+                <div className="FooterLink" to="/login">
+                  Contact
+                </div>
+                <div className="FooterLink" to="/login">
+                  Support
+                </div>
+                <div className="FooterLink" to="/login">
+                  Destinations
+                </div>
+                <div className="FooterLink" to="/login">
+                  Sponsorships
+                </div>
+              </div>
+            </div>
+            <div className="FooterLinksWrapper">
+              <div className="FooterLinkItems">
+                <div className="FooterLinkTitle">Videos</div>
+                <div className="FooterLink" to="/login">
+                  Submit Video
+                </div>
+                <div className="FooterLink" to="/login">
+                  Ambassadors
+                </div>
+                <div className="FooterLink" to="/login">
+                  Agency
+                </div>
+                <div className="FooterLink" to="/login">
+                  Influencer
+                </div>
+              </div>
+              <div className="FooterLinkItems">
+                <div className="FooterLinkTitle">Social Media</div>
+                <div className="FooterLink" to="/login">
+                  Instagram
+                </div>
+                <div className="FooterLink" to="/login">
+                  Facebook
+                </div>
+                <div className="FooterLink" to="/login">
+                  Youtube
+                </div>
+                <div className="FooterLink" to="/login">
+                  Twitter
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="SocialMedia">
+            <div className="SocialMediaWrap">
+              <div className="SocialLogo" to="/" onClick={toggleHome}>
+                BCI
+              </div>
+              <div className="WebsiteRights">
+                BCI © {new Date().getFullYear()} All rights reserved.
+              </div>
+              <div className="SocialIcons">
+                <div
+                  className="SocialIconLink"
+                  href="/"
+                  target="_blank"
+                  aria-label="Facebook"
+                >
+                  <FaFacebook />
+                </div>
+                <div
+                  className="SocialIconLink"
+                  href="/"
+                  target="_blank"
+                  aria-label="Instagram"
+                >
+                  <FaInstagram />
+                </div>
+                <div
+                  className="SocialIconLink"
+                  href="/"
+                  target="_blank"
+                  aria-label="Youtube"
+                >
+                  <FaYoutube />
+                </div>
+                <div
+                  className="SocialIconLink"
+                  href="/"
+                  target="_blank"
+                  aria-label="Twitter"
+                >
+                  <FaTwitter />
+                </div>
+                <div
+                  className="SocialIconLink"
+                  href="/"
+                  target="_blank"
+                  aria-label="Linkedin"
+                >
+                  <FaLinkedin />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
